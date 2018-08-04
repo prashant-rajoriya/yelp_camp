08e18d1715ffd45949393c5caf10eb34fd4ed42b
Prashant Rajoriya <prashant.rajoriya21@gmail.com>
2018-08-04 20:39:03 +0530

    Adding COntact Form using nodemailer

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/user');
 const passport = require('passport');
-
+const nodemailer = require('nodemailer');
+const transporter = require('../middleware/nodemailer')
 //---------Redirect to Campgrounds Page----------
 router.get('/', (req, res) => {
   res.render('landing');
@@ -46,6 +47,41 @@ router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success', 'You have been logged out.');
   res.redirect('/campgrounds');
+});
+
+//-----------Get Contact Form--------------------------
+router.get('/contact', (req, res) =>{
+  res.render('contact');
+});
+
+//-----------Sent Email Using nodemailer---------------
+router.post('/contact', (req, res) => {
+
+    let output = `
+      <p> ${req.body.contact.name} send you a mail</p>
+      <h3>Message to be recieved</h3>
+      <ul>
+        <li> Name : ${req.body.contact.name}</li>
+        <li> Email : ${req.body.contact.email}</li>
+        <li> Phone : ${req.body.contact.phone}</li>
+        <li> Company : ${req.body.contact.company}</li>
+      </ul>
+      <p>${req.body.contact.message}</p>
+    `;
+
+    const mailOptions = {
+      from : 'opfhz3ehocdjxj3o@ethereal.email',
+      to: 'prashant.ulo@gmail.com',
+      subject: 'Node mailer test mail',
+      html: output
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if(err){
+        console.error(err);
+      }
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
